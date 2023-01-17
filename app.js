f4e3068e9f8b7e1d011a8f3de2b5e52801db153e
Jeric <marianovjeric@gmail.com>
2023-01-17 00:25:20 -0800

    using the passed argument in the firstSixIncomplete function

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ async function firstSixIncomplete(todos) {
   const promise = await fetch("https://jsonplaceholder.typicode.com/todos");
   const result = (await promise.json())
 
-  const incompleteTasks = result.filter(element => !element.completed).slice(0, 6)
+  const incompleteTasks = result.filter(element => !element.completed).slice(0, todos)
 
   console.log(incompleteTasks)
 }
 
-firstSixIncomplete(4);
+firstSixIncomplete(6);
